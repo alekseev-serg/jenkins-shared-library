f4e69368096825f2af91af727d4bb6b24e6fd1af
sergey.alekseev <demiurg58@gmail.com>
2025-05-01 14:06:59 +0300

    example of library

--- src/ru/devops/Script.groovy
+++ src/ru/devops/Script.groovy
@@ -1,2 +1,14 @@
 // write Class
 package ru.devops
+
+class Script implements Serializable {
+    def steps;
+
+    Script(steps){
+        this.steps = steps;
+    }
+
+    def greet(String name){
+        steps.echo "Hello, $name from class"
+    }
+}
--- vars/buildNpm.groovy
+++ vars/buildNpm.groovy
@@ -2,5 +2,12 @@ import ru.devops.*
 
 
 def call(){
-    // write script
+    stage('Build'){
+        echo "Hello from Vars library!";
+    }
+
+    stage('From Class'){
+        def helper  = new Script(this)
+        helper.greet('Devops')
+    }
 }
